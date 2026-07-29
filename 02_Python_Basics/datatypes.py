bf45a959c34b8ee37cdafe4386a58ebe882911eb
Sachin DK <sachindiazsrz@gmail.com>
2026-07-29 23:01:22 +0530

    Complete datatypes lesson

---
+++ 02_Python_Basics/datatypes.py
@@ -0,0 +1,21 @@
+name = "Sachin"
+age = 24
+height = 5.6
+is_student = True
+
+print(name)
+print(age)
+print(height)
+print(is_student)
+
+#type()
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
+
+
+int()      # Convert to integer
+float()    # Convert to float
+str()      # Convert to string
+bool()     # Convert to boolean
