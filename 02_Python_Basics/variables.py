83f9eb899eead003aa50229525c5bd8fbb462ca7
Sachin DK <sachindiazsrz@gmail.com>
2026-07-29 22:42:04 +0530

    Complete variables lesson

---
+++ 02_Python_Basics/variables.py
@@ -0,0 +1,21 @@
+print("Hello, python!!!")
+print("My Name is Sachin")
+print("Im learning Python")
+print("Welcome to Python Fullstack Sprint")
+
+
+name = "sachin"
+age = "24"
+course = "Python fullstack"
+print(name)
+print(age)
+print(course)
+
+name = "rohit"
+age = "20"
+course = "python fullstack"
+
+print(name)
+print(age)
+print(course)
+print("python fullstack sprint")
