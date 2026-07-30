078846fdd89bef3bf110e6d11ec4a6da8e63339f
Sachin DK <sachindiazsrz@gmail.com>
2026-07-30 13:21:41 +0530

    Complete if statement lesson

---
+++ 02_Python_Basics/06_if_statement.py
@@ -0,0 +1,20 @@
+#if statement
+name = "sachin"
+age = 20 
+if age >= 18:
+    print("Welcome to Indian Election")
+    print("you can vote",name)
+
+marks = 80
+if marks >= 40:
+    print("pass")
+print("exam over🎉")
+
+age = 20
+
+if age >= 18:
+    print("You can vote.")
+    print("You can apply for a driving license.")
+    print("You are an adult.")
+
+print("Program Ended.")
