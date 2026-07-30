3624e93b1fb14bb873a4d7a414b69faa949579d3
Sachin DK <sachindiazsrz@gmail.com>
2026-07-30 12:51:53 +0530

    Complete operators lesson

---
+++ 02_Python_Basics/05_operators.py
@@ -0,0 +1,41 @@
+a = 5
+b = 10
+
+print("addition :",a + b)
+print("subtraction :",a - b)
+print("multiplication :",a * b)
+print("division :",a / b)
+print("remainder :",a % b)
+print("floor division :",a // b)
+print("power :",a ** b)
+
+#True or False
+a = 20
+b = 30 
+c = 30
+print(a == b)
+print(b == c)
+
+a = 15
+b = 10
+
+print(a > b)
+print(a < b)
+print(a == b)
+print(a != b)
+
+#logical operators
+age = 20
+has_license = True
+
+print(age >=18 and has_license)
+
+age = 17
+has_license = True
+
+print(age >=18 and has_license)
+
+print(5 > 2 and 10 < 2)
+print(10 > 5 and 8 < 3)
+
+print(5 > 10 or 7 == 7)
