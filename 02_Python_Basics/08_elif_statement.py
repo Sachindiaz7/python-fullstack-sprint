d42bccc0ba0c4a6619d0529a1eec4c67757fcbb9
Sachin DK <sachindiazsrz@gmail.com>
2026-07-30 14:18:46 +0530

    Complete elif statement lesson

---
+++ 02_Python_Basics/08_elif_statement.py
@@ -0,0 +1,40 @@
+#elif statement
+marks = 82
+if marks >=90:
+    print("grade A")
+elif marks >=75:
+    print("grade B")
+elif marks >= 50:
+    print("grade C")
+else:
+    print("failed.😪")
+
+marks = 42
+if marks >=90:
+    print("grade A")
+elif marks >=75:
+    print("grade B")
+elif marks >= 50:
+    print("grade C")
+else:
+    print("failed.")
+
+age = 18
+
+if age > 18:
+    print("Adult")
+elif age == 18:
+    print("Just became an Adult")
+else:
+    print("Minor")
+
+number = 0
+
+if number > 0:
+    print("Positive")
+elif number < 0:
+    print("Negative")
+else:
+    print("Zero")
+
+print("Program Finished")
