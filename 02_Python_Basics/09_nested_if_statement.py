8215371c68c6463fd62b1de45fa590a28758e43b
Sachin DK <sachindiazsrz@gmail.com>
2026-07-30 14:40:41 +0530

    Complete nested if statement lesson

---
+++ 02_Python_Basics/09_nested_if_statement.py
@@ -0,0 +1,31 @@
+age = 29
+has_lincense = True
+if age >= 18:
+    if has_lincense:
+        print("you can Drive")
+print("program ended")
+
+age = 22
+has_id = False
+if age >=18:
+    if has_id:
+        print("entry allowed")
+print("security check completed")
+
+age = 25
+has_id = True
+if age >=18:
+    print("age verified")
+    if has_id:
+        print("ID verified")
+print("welcome")
+
+marks = 80
+attendance = 90
+if marks >= 70:
+    print("mark passed")
+
+    if attendance >= 80:
+        print("attendance passed")
+
+print("result published")
