677b94f5101030a7e97184257e1714351265496c
Sachin DK <sachindiazsrz@gmail.com>
2026-07-30 15:11:25 +0530

    Complete for loop lesson

---
+++ 02_Python_Basics/11_for_loop.py
@@ -0,0 +1,31 @@
+#for loop
+for i in range(5):
+    print("its nice")
+
+for i in range(6):
+    print(i)
+
+for i in range(2,6):
+    print(i)
+
+for i in range(2,11,2):
+    print(i)
+
+for i in range(1, 10, 3):
+    print(i)
+
+for i in range(10,0,-2):
+    print(i)
+
+for i in range(1,6):
+    print(5 * i)
+
+for ch in "python":
+    print(ch)
+
+word = "Code"
+
+for letter in word:
+    print(letter)
+
+print("Completed")
