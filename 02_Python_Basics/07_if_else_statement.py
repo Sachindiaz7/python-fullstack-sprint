644bd345cd699161863f094a87976e6f6197c5e5
Sachin DK <sachindiazsrz@gmail.com>
2026-07-30 13:53:27 +0530

    Complete if else statement lesson

---
+++ 02_Python_Basics/07_if_else_statement.py
@@ -0,0 +1,47 @@
+#if else statement
+
+age = 15
+if age >=18 :
+    print("your eligible for vote✅")
+else:
+    print("your not eligible for vote❌")
+
+temperature = 35
+if temperature < 30:
+    print("pleasent weather")
+else:
+    print("hot weather")
+
+username = "Sachin"
+
+if username == "Sachin":
+    print("Welcome, Sachin!")
+else:
+    print("Access Denied")
+
+print("Login Completed")
+
+number = 7
+if number % 2 == 0:
+    print("even")
+else:
+    print("odd")
+print("done")
+
+marks = 75
+attendance = 85
+if marks >=50 and attendance >=80:
+    print("all pass subjects")
+else:
+    print("arrear in subjects")
+
+print("next semester")
+
+marks = 75
+attendance = 64
+if marks >=50 and attendance >=80:
+    print("all pass subjects")
+else:
+    print("arrear in subjects")
+
+print("next semester")
