900f429bdb9bcac46c9056e2b2e9d36b720e2ffe
Sachin DK <sachindiazsrz@gmail.com>
2026-07-30 17:26:39 +0530

    Complete while  loop lesson

---
+++ 02_Python_Basics/12_while_loop.py
@@ -0,0 +1,23 @@
+count = 1
+while count <= 5:
+    print(count)
+    count = count + 1
+
+x = 5
+
+while x >= 1:
+    print(x)
+    x = x - 1
+
+count = 2
+
+while count <= 8:
+    print(count)
+    count = count + 2
+
+num = 10
+while num >=2:
+    print(num)
+    num = num - 2
+print("loop finished")
+
---
+++ 02_Python_Basics/13_break_continue_pass.py
@@ -0,0 +1,47 @@
+#break
+for i in range(1,6):
+    print(i)
+
+    if i == 3:
+        break
+
+count = 1
+
+while count <= 5:
+    if count == 4:
+        break
+
+    print(count)
+    count = count + 1
+
+print("Loop Ended")
+
+#continue
+for i in range(1,6):
+    if i == 3:
+        continue
+
+    print(i)
+
+for i in range(1,8):
+    if i == 5:
+        continue
+    print(i)
+
+#break
+for i in range(1,6):
+    if i == 3:
+        break
+    print(i)
+
+count = 1
+
+while count <= 5:
+    count = count + 1
+
+    if count == 3:
+        continue
+
+    print(count)
+
+print("Done")
