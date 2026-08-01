8669a1c6dd5f8412a24a1a273e941131470db32a
Sachin DK <sachindiazsrz@gmail.com>
2026-08-01 19:47:58 +0530

    Complete string part 2 lesson

---
+++ 02_Python_Basics/14_strings_part_2.py
@@ -0,0 +1,24 @@
+# in , not in membership operator
+
+word = "programming"
+print("p" in word)
+
+word = "programming"
+print("z" in word)
+
+word = "Computer"
+
+print("m" not in word)
+print("x" not in word)
+print("r" not in word)
+
+#character in string (ch)
+name = "developer"
+for ch in name:
+    print(ch)
+
+name = "AI"
+for ch in name:
+    print(ch)
+
+print("Completed")
